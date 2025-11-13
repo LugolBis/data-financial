95d012a1cf8b1a3ac8876abc2fb6cf78e4a2d23a
LugolBis <ld.desmares@gmail.com>
2025-11-13 12:17:53 +0100

    Improved data summary

--- src/data_summary.py
+++ src/data_summary.py
@@ -2,35 +2,42 @@ import duckdb
 from generate_query import get_files_path, generate_duckdb_query
 import os
 
-def stat_groupby(subquery: str) -> None:
+def stat_summary(folder: str, subquery: str) -> None:
+    path: str = os.path.join(folder, "summary.csv")
+    query: str = f"""COPY (SELECT * FROM (SUMMARIZE {subquery})) TO '{path}' (HEADER, DELIMITER ';');"""
+
+    duckdb.execute(query)
+
+def stat_currencies(folder: str, subquery: str) -> None:
+    path: str = os.path.join(folder, "ratio.csv")
     query: str = \
-        f"""SELECT payment_currency as Currency, MEAN(exchange), MIN(exchange), MAX(exchange) \
-            FROM ({subquery}) \
-            GROUP BY payment_currency;
+        f"""COPY (
+                WITH subquery as (
+                    {subquery}
+                ),
+                ratio as (
+                    SELECT payment_currency, date_trans, amount_paid, (amount_paid - exchange) as ratio
+                    FROM subquery
+                ),
+                computed as (
+                    SELECT
+                        s.payment_currency as currency,
+                        MEAN(r.ratio) as mean_ratio,
+                        MIN(r.ratio) as min_ratio,
+                        MAX(r.ratio) as max_ratio,
+                        SUM(r.ratio) as sum_ratio
+                    FROM subquery s INNER JOIN ratio r 
+                        ON r.payment_currency = s.payment_currency
+                        AND r.date_trans = s.date_trans
+                        AND r.amount_paid = s.amount_paid
+                    GROUP BY s.payment_currency
+                )
+            
+                SELECT * FROM computed
+            ) TO '{path}' (HEADER, DELIMITER ';');
         """
     
-    duckdb.sql(query).show()
-
-def stat_currencies(subquery: str) -> None:
-    query: str = \
-        f"""WITH ratio as (
-                SELECT payment_currency, date_trans, amount_paid, (amount_paid - exchange) as ratio
-                FROM ({subquery})
-            ),
-            computed as (
-                SELECT s.payment_currency as Currency, MEAN(r.ratio), MIN(r.ratio), MAX(r.ratio), SUM(r.ratio)
-                FROM ({subquery}) s INNER JOIN ratio r 
-                    ON r.payment_currency = s.payment_currency
-                    AND r.date_trans = s.date_trans
-                    AND r.amount_paid = s.amount_paid
-                GROUP BY s.payment_currency
-            )
-
-            SELECT *
-            FROM computed;
-        """
-    
-    duckdb.sql(query).show()
+    duckdb.execute(query)
 
 if __name__ == "__main__":
     current_folder = os.path.curdir
@@ -42,5 +49,5 @@ if __name__ == "__main__":
     print("\nIn all the following data the column 'exchange' is the exchange between the currencies and the US Dollar.")
     print("Moreover, keep in mind that the transactions are extracted from a Dataset of Transactions for AML.\n")
 
-    stat_groupby(subquery)
-    stat_currencies(subquery)
+    #stat_summary(data_folder, subquery)
+    stat_currencies(data_folder, subquery)
