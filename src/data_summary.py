6017dc968ce54c44986d5a9b399a99b27d62db85
LugolBis <ld.desmares@gmail.com>
2025-11-13 11:30:26 +0100

    Added a function for data summary

--- src/data_summary.py
+++ src/data_summary.py
@@ -11,6 +11,27 @@ def stat_groupby(subquery: str) -> None:
     
     duckdb.sql(query).show()
 
+def stat_currencies(subquery: str) -> None:
+    query: str = \
+        f"""WITH ratio as (
+                SELECT payment_currency, date_trans, amount_paid, (amount_paid - exchange) as ratio
+                FROM ({subquery})
+            ),
+            computed as (
+                SELECT s.payment_currency as Currency, MEAN(r.ratio), MIN(r.ratio), MAX(r.ratio), SUM(r.ratio)
+                FROM ({subquery}) s INNER JOIN ratio r 
+                    ON r.payment_currency = s.payment_currency
+                    AND r.date_trans = s.date_trans
+                    AND r.amount_paid = s.amount_paid
+                GROUP BY s.payment_currency
+            )
+
+            SELECT *
+            FROM computed;
+        """
+    
+    duckdb.sql(query).show()
+
 if __name__ == "__main__":
     current_folder = os.path.curdir
     data_folder = os.path.join(current_folder, "Data", "transformed")
@@ -22,3 +43,4 @@ if __name__ == "__main__":
     print("Moreover, keep in mind that the transactions are extracted from a Dataset of Transactions for AML.\n")
 
     stat_groupby(subquery)
+    stat_currencies(subquery)
